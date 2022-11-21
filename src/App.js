4255a0712c74ffcbd9c7daa76ba025412c18316f
Sebastian Molina <semolina@utp.edu.co>
2022-11-21 17:53:52 -0500

    Pull

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Perrito :3 AAAAAAAAAA
+          Celeste
         </a>
       </header>
     </div>
