9d121e7432c230215f333d8235bf0b87c1e02d48
Sebastian Molina <semolina@utp.edu.co>
2022-11-21 16:03:17 -0500

    Si señor

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          wweweqw Learn React xD mira vos
+          Learn React
         </a>
       </header>
     </div>
