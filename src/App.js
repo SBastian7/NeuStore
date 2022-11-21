4f5da37643c6163345a56bd3465d31de388b7125
Sebastian Molina <semolina@utp.edu.co>
2022-11-21 15:15:50 -0500

    chasng port

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React xD
+          Learn React xD mira vos
         </a>
       </header>
     </div>
